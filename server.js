d8ec908e4c1086caefd64accbd40c125baadd37e
ahmed0ty <ahmedabdelbaky2321@gmail.com>
2026-05-09 18:09:03 +0300

    sdfds

--- server.js
+++ server.js
@@ -334,9 +334,10 @@ io.on("connection", (socket) => {
     io.emit("chat-cleared");
   });
 
- socket.on("stream-offer", (data) => {
+  socket.on("stream-offer", (data) => {
+    if (!users[socket.id]) return;
     socket.broadcast.emit("stream-offer", { offer: data.offer, from: socket.id });
-});
+  });
 
   socket.on("stream-answer", (data) => {
     socket.broadcast.emit("stream-answer", { answer: data.answer });
